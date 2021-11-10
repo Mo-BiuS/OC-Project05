c73520cb8ef619d48290cdfa75e5066b3f679510
Mo-BiuS <lodland95@gmail.com>
2021-11-10 16:20:32 +0100

    Cleaning

--- src/main/java/com/safetyNet/Alerts/AlertsApplication.java
+++ src/main/java/com/safetyNet/Alerts/AlertsApplication.java
@@ -1,13 +1,28 @@
 package com.safetyNet.Alerts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AlertsApplication {
+import com.safetyNet.Alerts.Model.Person;
+import com.safetyNet.Alerts.Service.BusinessService;
 
+@SpringBootApplication
+public class AlertsApplication implements CommandLineRunner{
+
+	@Autowired
+	private BusinessService bs;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AlertsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Person hw = bs.getHelloWorld();
+		
+		System.out.println(hw);
 	}
 
 }
