3c3c0e1f53d9c9c4eb289033f20c592bae93d299
Mo-BiuS <lodland95@gmail.com>
2021-12-16 10:21:39 +0100

    Service logger

--- src/main/java/com/safetyNet/Alerts/Service/DataHandler.java
+++ src/main/java/com/safetyNet/Alerts/Service/DataHandler.java
@@ -4,13 +4,21 @@ import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetyNet.Alerts.Model.Data;
 
 public class DataHandler {
+
+	private static final Logger logger = LogManager.getLogger("DataHandler");
     private static Data DATA = processData();
     
     private static Data processData() {
+    	
+    	logger.info("Loading data.json : ");
+    	
         Data DATA;
         try {
             ObjectMapper mapper = new ObjectMapper();
@@ -18,7 +26,9 @@ public class DataHandler {
             mapper.setDateFormat(df);
             
             DATA = mapper.readValue(new File("./src/main/resources/data.json"), Data.class);
+            logger.info("Success ");
         } catch (Exception ex) {
+        	logger.info("Failure " + ex.toString());
             ex.printStackTrace();
             DATA = null;
         }
@@ -34,6 +44,7 @@ public class DataHandler {
     }
     
     public static void loadData(Data data) {
+    	logger.info("FORCE FEEDING DATA");
     	DATA = data;
     }
 }
