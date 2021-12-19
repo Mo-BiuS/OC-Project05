df5fe1db1fb700204e6621471066c1fe773f606e
Mo-BiuS <lodland95@gmail.com>
2021-12-19 17:42:31 +0100

    Integration tests A

--- src/main/java/com/safetyNet/Alerts/Controller/AppController.java
+++ src/main/java/com/safetyNet/Alerts/Controller/AppController.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,7 @@ public class AppController {
 //	doit inclure les informations spécifiques suivantes : prénom, nom, adresse, numéro de téléphone. De plus, 
 //	elle doit fournir un décompte du nombre d'adultes et du nombre d'enfants (tout individu âgé de 18 ans ou 
 //	moins) dans la zone desservie.
-	@GetMapping("/firestation")
+	@RequestMapping("/firestation")
 	@ResponseBody
 	public ReqFirestation firestation(int stationNumber) { 
 		logger.info("Requesting : /firestation?stationNumber="+stationNumber);
@@ -38,7 +39,7 @@ public class AppController {
 //	Cette url doit retourner une liste d'enfants (tout individu âgé de 18 ans ou moins) habitant à cette adresse. 
 //	La liste doit comprendre le prénom et le nom de famille de chaque enfant, son âge et une liste des autres 
 //	membres du foyer. S'il n'y a pas d'enfant, cette url peut renvoyer une chaîne vide
-	@GetMapping("/childAlert")
+	@RequestMapping("/childAlert")
 	@ResponseBody
 	public ReqChildAlert childAlert(String address) {
 		logger.info("Requesting : /childAlert?address="+address);
@@ -49,7 +50,7 @@ public class AppController {
 	
 //	Cette url doit retourner une liste des numéros de téléphone des résidents desservis par la caserne de 
 //	pompiers. Nous l'utiliserons pour envoyer des messages texte d'urgence à des foyers spécifiques. 
-	@GetMapping("/phoneAlert")
+	@RequestMapping("/phoneAlert")
 	@ResponseBody
 	public List<String> phoneAlert(int stationNumber) { 
 		logger.info("Requesting : /phoneAlert?stationNumber="+stationNumber);
@@ -61,7 +62,7 @@ public class AppController {
 //	Cette url doit retourner la liste des habitants vivant à l’adresse donnée ainsi que le numéro de la caserne 
 //	de pompiers la desservant. La liste doit inclure le nom, le numéro de téléphone, l'âge et les antécédents 
 //	médicaux (médicaments, posologie et allergies) de chaque personne. 
-	@GetMapping("/fire")
+	@RequestMapping("/fire")
 	@ResponseBody
 	public ReqFire fire(String address) { 
 		logger.info("Requesting : /fire?address="+address);
@@ -73,9 +74,9 @@ public class AppController {
 //	Cette url doit retourner une liste de tous les foyers desservis par la caserne. Cette liste doit regrouper les 
 //	personnes par adresse. Elle doit aussi inclure le nom, le numéro de téléphone et l'âge des habitants, et 
 //	faire figurer leurs antécédents médicaux (médicaments, posologie et allergies) à côté de chaque nom. 
-	@GetMapping("/flood/stations")
+	@RequestMapping("/flood/stations/{stations}")
 	@ResponseBody
-	public ReqFloodStations reqFloodStations(List<Integer> stations) { 
+	public ReqFloodStations reqFloodStations(@PathVariable List<Integer> stations) { 
 		logger.info("Requesting : /flood/stations?stations="+stations);
 		ReqFloodStations req = RequestService.reqFloodStations(stations);
 		logger.info("Returning : "+req);
@@ -85,7 +86,7 @@ public class AppController {
 //	Cette url doit retourner le nom, l'adresse, l'âge, l'adresse mail et les antécédents médicaux (médicaments, 
 //	posologie, allergies) de chaque habitant. Si plusieurs personnes portent le même nom, elles doivent 
 //	toutes apparaître. 
-	@GetMapping("/personInfo")
+	@RequestMapping("/personInfo")
 	@ResponseBody
 	public ReqPersonInfo personInfo(String firstName, String lastName) {
 		logger.info("Requesting : /personInfo?firstName="+firstName+"&lastName="+lastName);
@@ -95,7 +96,7 @@ public class AppController {
 	}
 	
 //	Cette url doit retourner les adresses mail de tous les habitants de la ville.
-	@GetMapping("/communityEmail")
+	@RequestMapping("/communityEmail")
 	@ResponseBody
 	public List<String> communityEmail(@RequestParam(required = true) String city) {
 		logger.info("Requesting : /communityEmail?city="+city);
--- src/test/java/com/safetyNet/Alerts/Integration/AppControllerTest.java
+++ src/test/java/com/safetyNet/Alerts/Integration/AppControllerTest.java
@@ -1,4 +1,207 @@
 package com.safetyNet.Alerts.Integration;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.safetyNet.Alerts.Model.Data;
+import com.safetyNet.Alerts.Model.Firestation.Firestation;
+import com.safetyNet.Alerts.Model.Medicalrecord.Medicalrecord;
+import com.safetyNet.Alerts.Model.Person.Person;
+import com.safetyNet.Alerts.Service.DataHandler;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AppControllerTest {
+	public static String adressAB;
+	public static String adressC;
+	
+	public static int stationA;
+	public static int stationB;
+	
+	public static Firestation firestationA;
+	public static Firestation firestationB;
+	
+	public static String firstNameA;
+	public static String firstNameB;
+	public static String firstNameC;
+	
+	public static String lastNameA;
+	public static String lastNameB;
+	public static String lastNameC;
+	
+	public static Date birthdateA;
+	public static Date birthdateB;
+	public static Date birthdateC;
+	
+	public static ArrayList<String> medicationsA;
+	public static ArrayList<String> medicationsB;
+	public static ArrayList<String> medicationsC;
+	
+	public static ArrayList<String> allergiesA;
+	public static ArrayList<String> allergiesB;
+	public static ArrayList<String> allergiesC;
+	
+	public static Medicalrecord medicalRecordTestA;
+	public static Medicalrecord medicalRecordTestB;
+	public static Medicalrecord medicalRecordTestC;
+	
+	public static String city;
+	
+	public static String phoneA;
+	public static String phoneB;
+	public static String phoneC;
+	
+	public static String emailA;
+	public static String emailB;
+	public static String emailC;
+	
+	public static short zipAB;
+	public static short zipC;
+	
+	public static Person personA;
+	public static Person personB;
+	public static Person personC;
+	
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeAll
+	public static void init() throws ParseException {
+		birthdateA = new SimpleDateFormat("dd/MM/yyyy").parse("19/01/2005");
+		birthdateB = new SimpleDateFormat("dd/MM/yyyy").parse("20/05/1995");
+		birthdateC = new SimpleDateFormat("dd/MM/yyyy").parse("22/01/1985");
+		
+		medicationsA = new ArrayList<String>();
+		medicationsA.add("aznol:350");
+		medicationsA.add("hydrapermazol:100");
+		medicationsB = new ArrayList<String>();
+		medicationsC = new ArrayList<String>();
+		medicationsC.add("aznol:350");
+		
+		allergiesA = new ArrayList<String>();
+		allergiesB = new ArrayList<String>();
+		allergiesB.add("ork");
+		allergiesC = new ArrayList<String>();
+		allergiesC.add("peanut");
+		
+		medicalRecordTestA = new Medicalrecord(firstNameA = "a", lastNameA = "A", birthdateA, medicationsA, allergiesA);
+		medicalRecordTestB = new Medicalrecord(firstNameB = "b", lastNameB = "B", birthdateB, medicationsB, allergiesB);
+		medicalRecordTestC = new Medicalrecord(firstNameC = "c", lastNameC = "C", birthdateC, medicationsC, allergiesC);
+		
+		personA = new Person(firstNameA, lastNameA, adressAB = "1A", city = "city", zipAB = 1, phoneA = "06", emailA = "a@a");
+		personB = new Person(firstNameB, lastNameB, adressAB = "1A", city, zipAB = 1, phoneB = "07", emailB = "b@b");
+		personC = new Person(firstNameC, lastNameC, adressC = "1B", city, zipC = 2, phoneC = "08", emailC = "c@c");
+		
+		firestationA = new Firestation(adressAB, stationA = 1);
+		firestationB = new Firestation(adressC, stationB = 2);
+
+		List<Firestation> lFirestation = new ArrayList<Firestation>();
+		lFirestation.add(firestationA);
+		lFirestation.add(firestationB);
+		
+		List<Person> lPerson = new ArrayList<Person>();
+		lPerson.add(personA);
+		lPerson.add(personB);
+		lPerson.add(personC);
+		
+		List<Medicalrecord> lMedicalrecord = new ArrayList<Medicalrecord>();
+		lMedicalrecord.add(medicalRecordTestA);
+		lMedicalrecord.add(medicalRecordTestB);
+		lMedicalrecord.add(medicalRecordTestC);
+		
+		DataHandler.loadData(new Data(lPerson, lFirestation, lMedicalrecord));
+	}
+	
+	@Test
+	public void firestation() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/firestation?stationNumber="+stationA))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(firstNameA)))
+					.andExpect(content().string(containsString(lastNameA)))
+					.andExpect(content().string(containsString(firstNameB)))
+					.andExpect(content().string(containsString(lastNameB)))
+					.andExpect(content().string(containsString("\"adultCount\":1")))
+					.andExpect(content().string(containsString("\"childCount\":1")));
+	}
+	
+	@Test
+	public void childAlert() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/childAlert?address="+adressAB))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(firstNameA)))
+					.andExpect(content().string(containsString(lastNameA)))
+					.andExpect(content().string(containsString(firstNameB)))
+					.andExpect(content().string(containsString(lastNameB)));
+	}
+	
+	@Test
+	public void phoneAlert() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/phoneAlert?stationNumber="+stationA))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(phoneA)))
+					.andExpect(content().string(containsString(phoneB)));
+	}
+	
+	@Test
+	public void fire() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/fire?address="+adressAB))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(firstNameA)))
+					.andExpect(content().string(containsString(lastNameA)))
+					.andExpect(content().string(containsString(firstNameB)))
+					.andExpect(content().string(containsString(lastNameB)))
+					.andExpect(content().string(containsString("\"stationsNumber\":["+stationA)));
+	}
+	
+	@Test
+	public void reqFloodStations() throws Exception {
+		
+		this.mockMvc.perform(get("http://localhost:9090/flood/stations/"+stationA))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(firstNameA)))
+					.andExpect(content().string(containsString(lastNameA)))
+					.andExpect(content().string(containsString(firstNameB)))
+					.andExpect(content().string(containsString(lastNameB)));
+	}
+	
+	@Test
+	public void personInfo() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/personInfo?firstName="+firstNameA+"&lastName="+lastNameA))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(firstNameA)))
+					.andExpect(content().string(containsString(lastNameA)));
+	}
+	
+	@Test
+	public void communityEmail() throws Exception {
+		this.mockMvc.perform(get("http://localhost:9090/communityEmail?city="+city))
+					.andDo(print())
+					.andExpect(status().isOk())
+					.andExpect(content().string(containsString(emailA)))
+					.andExpect(content().string(containsString(emailB)))
+					.andExpect(content().string(containsString(emailC)));
+	}
 }
